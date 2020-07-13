a751fa19da40eaab481173fa4a8ffcfa36d9d56f
Andrew Wong <andrew.j.wong@outlook.com>
2020-07-13 16:34:08 +1000

    Create file-ext-filter-bypass.php.c

---
+++ blog/static/post/report1/file-ext-filter-bypass.php.c
@@ -0,0 +1,20 @@
+/*
+
+Woah secret stuff!
+
+Trust me, it's not base64...
+QXJiaXRyYXJ5IGRhdGEgZ29lcyBoZXJlOiBWMjkzSUdseklIUm9hWE1nY21WamRYSnphVzl1UHlBNk9pQldNamt6U1Vkc2VrbElVbTloV0UxblkyMVdhbVJZU25waFZ6bDFVSGxCTms5cFFsZE5hbXQ2VTFWa2MyVnJiRWxWYlRsb1YwVXhibGt5TVZkaGJWSlpVMjV3YUZaNmJERlZTR001VUZFOVBRPT0=
+
+
+⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿
+⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+*/
+
+int main() {}
