838d5f9316ff558930c3782c48cbbc24576743c0
Andrew Wong <andrew.j.wong@outlook.com>
2020-07-14 17:11:41 +1000

    Update file-ext-filter-bypass.php.c

--- blog/static/post/report1/file-ext-filter-bypass.php.c
+++ blog/static/post/report1/file-ext-filter-bypass.php.c
@@ -1,20 +1,19 @@
-/*
+char *data = ""
 
-Woah secret stuff!
+""
+"Woah secret stuff!"
+""
+"Trust me, it's not base64..."
+"QXJiaXRyYXJ5IGRhdGEgZ29lcyBoZXJlOiBWMjkzSUdseklIUm9hWE1nY21WamRYSnphVzl1UHlBNk9pQldNamt6U1Vkc2VrbElVbTloV0UxblkyMVdhbVJZU25waFZ6bDFVSGxCTms5cFFsZE5hbXQ2VTFWa2MyVnJiRWxWYlRsb1YwVXhibGt5TVZkaGJWSlpVMjV3YUZaNmJERlZTR001VUZFOVBRPT0="
+""
+""
+"⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶"
+"⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿"
+"⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿"
+"⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿"
+"⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿"
+"⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿"
+"⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿"
+"⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
 
-Trust me, it's not base64...
-QXJiaXRyYXJ5IGRhdGEgZ29lcyBoZXJlOiBWMjkzSUdseklIUm9hWE1nY21WamRYSnphVzl1UHlBNk9pQldNamt6U1Vkc2VrbElVbTloV0UxblkyMVdhbVJZU25waFZ6bDFVSGxCTms5cFFsZE5hbXQ2VTFWa2MyVnJiRWxWYlRsb1YwVXhibGt5TVZkaGJWSlpVMjV3YUZaNmJERlZTR001VUZFOVBRPT0=
-
-
-⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿
-⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-
-*/
-
-int main() {}
+;int main() {}
